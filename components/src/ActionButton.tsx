20445d596edd3ab4dc0938febc8d36074614424f
gregfromstl <gregfromstl@gmail.com>
2022-07-18 12:21:01 -0500

    feat(components): add ActionButton

---
+++ components/src/ActionButton.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useWaitForTransaction } from "wagmi";
+import { useAccount, useNetwork, usePreferredNetwork } from "@nation3/utils";
+import ActionNeedsTokenApproval from "./ActionNeedsTokenApproval";
+
+export default function ActionButton({
+  className,
+  children,
+  action,
+  preAction,
+  postAction,
+  approval,
+}: any) {
+  const { data: account } = useAccount();
+  const { writeAsync, data, isLoadingOverride } = action;
+  const { isLoading } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+  const onClick = async () => {
+    preAction && preAction();
+    const tx = await writeAsync();
+    tx?.wait && (await tx.wait());
+    postAction && postAction();
+  };
+
+  const { activeChain } = useNetwork({});
+  const { isPreferredNetwork } = usePreferredNetwork();
+
+  return (
+    <>
+      {!isPreferredNetwork ? (
+        <button className={className} disabled>
+          {!activeChain?.id ? "Not connected" : "Wrong network"}
+        </button>
+      ) : !account ? (
+        <label htmlFor="web3-modal" className={`${className} modal-button`}>
+          {children}
+        </label>
+      ) : isLoading || isLoadingOverride ? (
+        <div className={className}>
+          <button className="btn btn-square btn-link btn-disabled bg-transparent loading"></button>
+        </div>
+      ) : approval ? (
+        <ActionNeedsTokenApproval
+          className={className}
+          action={action}
+          preAction={preAction}
+          {...approval}
+        >
+          {children}
+        </ActionNeedsTokenApproval>
+      ) : (
+        <div className={className} onClick={onClick}>
+          {children}
+        </div>
+      )}
+    </>
+  );
+}
