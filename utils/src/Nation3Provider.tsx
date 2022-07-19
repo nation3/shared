9c5f2bc6c49bbe12a3d1758b454e34b01b3fd4a8
gregfromstl <gregfromstl@gmail.com>
2022-07-19 18:23:54 -0500

    feat(utils): add connectors

--- utils/src/Nation3Provider.tsx
+++ utils/src/Nation3Provider.tsx
@@ -1,10 +1,30 @@
 import { ErrorProvider } from "./ErrorProvider";
-import React from "react";
+import { connectors, provider as externalProvider } from "./connectors";
+import { ethers } from "ethers";
+import React, { useEffect } from "react";
 import { WagmiProvider } from "wagmi";
 
 export const Nation3Provider = ({ children }: React.PropsWithChildren<{}>) => {
+  let provider = externalProvider();
+
+  useEffect(() => {
+    const userProvider =
+      window.ethereum || (window as unknown as any).web3?.currentProvider;
+    if (userProvider && process.env.NEXT_PUBLIC_CHAIN !== "local") {
+      provider = new ethers.providers.Web3Provider(
+        userProvider,
+        process.env.NEXT_PUBLIC_CHAIN
+      );
+    }
+    console.log(provider);
+  }, []);
+
   return (
-    <WagmiProvider>
+    <WagmiProvider
+      connectors={connectors}
+      provider={provider}
+      autoConnect={true}
+    >
       <ErrorProvider>{children}</ErrorProvider>
     </WagmiProvider>
   );
---
+++ utils/src/connectors.ts
@@ -0,0 +1,47 @@
+import { config } from "./config";
+import { ethers } from "ethers";
+import { chain } from "wagmi";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+
+const chains = [chain.mainnet, chain.goerli, chain.hardhat];
+
+export function provider() {
+  if (process.env.NEXT_PUBLIC_CHAIN === "local") {
+    console.log("Provider: Connected to localhost provider");
+    return new ethers.providers.JsonRpcProvider(
+      "http://127.0.0.1:7545",
+      config.networks[process.env.NEXT_PUBLIC_CHAIN ?? "mainnet"]
+    );
+  } else {
+    console.log(
+      `Provider: Connected to the external provider on chain ${process.env.NEXT_PUBLIC_CHAIN}`
+    );
+    return ethers.getDefaultProvider(process.env.NEXT_PUBLIC_CHAIN, {
+      infura: process.env.NEXT_PUBLIC_INFURA_ID,
+      alchemy: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+      etherscan: process.env.NEXT_PUBLIC_ETHERSCAN_ID,
+      quorum: 1,
+    });
+  }
+}
+
+export const connectors = [
+  new InjectedConnector({
+    chains,
+    options: { shimDisconnect: true },
+  }),
+  new WalletConnectConnector({
+    chains,
+    options: {
+      qrcode: true,
+    },
+  }),
+  new CoinbaseWalletConnector({
+    chains,
+    options: {
+      appName: "Nation3 app",
+    },
+  }),
+];
