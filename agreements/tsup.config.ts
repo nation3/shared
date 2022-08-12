50c3f5211cb5b3f75ab87c1e7343f24c0b281e56
gregfromstl <gregfromstl@gmail.com>
2022-08-12 10:15:25 -0500

    fix(agreements): type compilation

---
+++ agreements/tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  sourcemap: true,
+  clean: true,
+  minify: true,
+  platform: "browser",
+  replaceNodeEnv: true,
+  // now required because not defaulted anymore
+  shims: true,
+  // use rollup for build to get smaller bundle sizes with tree shaking
+  treeshake: true,
+  globalName: "Nation3Components",
+  format: ["cjs", "esm"],
+  esbuildPlugins: [],
+  // inject globals onto window if required
+  banner: {
+    js: '!function(o){o&&(void 0===o.global&&(o.global=o),void 0===o.globalThis&&(o.globalThis=o),void 0===o.process&&(o.process={env:{NODE_ENV:"production"}}))}("undefined"!=typeof window?window:void 0);',
+  },
+});
--- preview/src/App.tsx
+++ preview/src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Badge } from "@nation3/agreements";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Badge value={"Hello"} />
         <a
           className="App-link"
           href="https://reactjs.org"
