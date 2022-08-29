bbaa69b8cc35bdda7a096173eb15e14523a4d62e
gregfromstl <gregfromstl@gmail.com>
2022-08-29 17:01:49 -0500

    fix(Table): increases text size

--- components/src/Tables/Table.tsx
+++ components/src/Tables/Table.tsx
@@ -8,9 +8,9 @@ interface TableProps {
 
 export default function Table({ columns, data, className }: TableProps) {
   return (
-    <div className={`relative overflow-x-auto ${className}`}>
-      <table className="w-full text-sm text-left text-gray-500">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+    <div className={`relative overflow-x-auto text-sm ${className}`}>
+      <table className="w-full text-left text-gray-500">
+        <thead className="text-gray-700 uppercase bg-gray-50 ">
           <tr>
             {columns.map((column) => (
               <th scope="col" className="px-6 py-3">
@@ -21,7 +21,7 @@ export default function Table({ columns, data, className }: TableProps) {
         </thead>
         <tbody className="text-black">
           {data.map((row) => (
-            <tr className="bg-white border-b ">
+            <tr className="bg-white border-b">
               {row.map((item) => (
                 <td className="px-6 py-4">{item}</td>
               ))}
