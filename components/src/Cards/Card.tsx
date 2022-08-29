20f56c586e116e937ef331b3cd9f87938a93ba0d
gregfromstl <gregfromstl@gmail.com>
2022-08-29 17:27:31 -0500

    feat(Card): enables adding className prop to card

--- components/src/Cards/Card.tsx
+++ components/src/Cards/Card.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Card({
   children,
+  className,
 }: {
   children: React.ReactElement | React.ReactElement[]
+  className?: string
 }) {
-  return <div className="p-6 bg-white rounded-lg shadow-md ">{children}</div>
+  return (
+    <div className={`p-6 bg-white rounded-lg shadow-md ${className}`}>
+      {children}
+    </div>
+  )
 }
